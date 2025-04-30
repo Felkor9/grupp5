600078aebe2509e050c2ed36121162d9d0721125
Evelina Jonasson <idaevelina97@gmail.com>
2025-04-30 12:55:33 +0200

    ResaLand DestinationStad DestinationHotell

--- backend/controllers/resorController.js
+++ backend/controllers/resorController.js
@@ -1,80 +1,112 @@
-const resorService = require('./../services/resorService');
+const resorService = require("./../services/resorService");
 
-exports.getResor = (async(req, res) => {
-    try {
-        const resor = await resorService.getResor();
-        res.json({resor});
-    }catch(error){
-        return res.status(500).json({
-            error: error.message
-        });
-    }
-});
+exports.getResor = async (req, res) => {
+  try {
+    const resor = await resorService.getResor();
+    res.json({ resor });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
-
-exports.getResa = (async(req, res) => {
+exports.getResa = async (req, res) => {
   const { id } = req.params;
 
   try {
-      const resor = await resorService.getResa(id);
-      res.json({resor});
-  }catch(error){
-      return res.status(500).json({
-          error: error.message
-      });
+    const resor = await resorService.getResa(id);
+    res.json({ resor });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
   }
-});
-
-exports.createResor = (async(req, res) => {
-  const { resorLand, resorDatum, resorLangd, resorPris} = req.body;
-
-  if (!resorLand || !resorDatum || !resorLangd || !resorPris) {
-      return res.status(400).json({
-          success: false,
-          error: 'Du har inte fyllt i fälten korrekt.',
-      });
-  }
-
-  try{
-      await resorService.createResor(resorLand, resorDatum, resorLangd, resorPris);
-      return res.status(201).json({
-          success: true,
-          error: '',
-          message: 'Du har lagt till en ny resa.'
-      });
-  }catch(error){
-      return res.status(500).json({
-          success: false,
-          error: error.message,
-      });
-  }
-});
-
-
-exports.updateResor = async (req, res) => {
-    try {
-        const resorId = req.params.id;
-        const resorData = req.body;
-
-        // Kontrollera att namn och plats finns
-        if (!resorData.resorLand || !resorData.resorDatum || !resorData.resaLangd || !resorData.resorPris) {
-            return res.status(400).json({ message: 'Alla värden måste anges' });
-        }
-
-        const updatedResor = await resorService.updateResor(resorId, resorData);
-        res.json(updatedResor);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
 };
 
+exports.createResor = async (req, res) => {
+  const { resorLand, resorDatum, resorLangd, resorPris } = req.body;
+
+  if (!resorLand || !resorDatum || !resorLangd || !resorPris) {
+    return res.status(400).json({
+      success: false,
+      error: "Du har inte fyllt i fälten korrekt.",
+    });
+  }
+
+  try {
+    await resorService.createResor(
+      resorLand,
+      resorDatum,
+      resorLangd,
+      resorPris
+    );
+    return res.status(201).json({
+      success: true,
+      error: "",
+      message: "Du har lagt till en ny resa.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.updateResor = async (req, res) => {
+  try {
+    const resorId = req.params.id;
+    const resorData = req.body;
+
+    // Kontrollera att namn och plats finns
+    if (
+      !resorData.resorLand ||
+      !resorData.resorDatum ||
+      !resorData.resaLangd ||
+      !resorData.resorPris
+    ) {
+      return res.status(400).json({ message: "Alla värden måste anges" });
+    }
+
+    const updatedResor = await resorService.updateResor(resorId, resorData);
+    res.json(updatedResor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 exports.deleteResa = async (req, res) => {
-    try {
-        const resorId = req.params.id;
-        const deleted = await resorService.deleteResa(resorId);
-        res.json(deleted);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const resorId = req.params.id;
+    const deleted = await resorService.deleteResa(resorId);
+    res.json(deleted);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// getResaDestination med specifikt ID
+exports.getResaDestination = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const resor = await resorService.getResaDestination(id);
+    res.json({ resor });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.getResorDestination = async (req, res) => {
+  try {
+    const resor = await resorService.getResorDestination();
+    res.json({ resor });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 };
--- backend/services/resorService.js
+++ backend/services/resorService.js
@@ -1,69 +1,96 @@
-const connectionMySQL = require('./../connectionMySQL');
+const connectionMySQL = require("./../connectionMySQL");
 
 function getResor() {
-    return new Promise((resolve, reject) => {
-        let sql = 'SELECT * FROM resor';
-        connectionMySQL.query(sql, (err, rows) => {
-            if(err)
-                reject(err);
-            else
-                resolve(rows);
-        });
+  return new Promise((resolve, reject) => {
+    let sql = "SELECT * FROM resor";
+    connectionMySQL.query(sql, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
     });
+  });
 }
 
 function getResa(id) {
   return new Promise((resolve, reject) => {
-      let sql = 'SELECT * FROM resor WHERE resorId = ?';
-      connectionMySQL.query(sql, [id], (err, rows) => {
-          if(err)
-              reject(err);
-          else
-              resolve(rows);
-      });
+    let sql = "SELECT * FROM resor WHERE resorId = ?";
+    connectionMySQL.query(sql, [id], (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
   });
 }
 
-function createResor(resorLand, resorDatum, resorLangd, resorPris){
-  return new Promise((resolve, reject)=>{
-      let sql= 'INSERT INTO resor (resorLand, resorDatum, resorLangd, resorPris) VALUES (?,?,?,?)'
-      let params = [resorLand, resorDatum, resorLangd, resorPris]
+// getResorDestination
+function getResorDestination() {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "SELECT resor.resorLand, destination.destinationStad, destination.destinationHotell FROM resor INNER JOIN destination ON resor.resorId = destination.destinationResorId";
+    connectionMySQL.query(sql, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
 
-      connectionMySQL.query(sql,params, (err)=>{
-          if(err)
-              reject(err)
-          else
-          resolve()
-      })
-  })
-};
+// getResaDestination med specifikt ID
+function getResaDestination(id) {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "SELECT resor.resorLand, destination.destinationStad, destination.destinationHotell FROM resor INNER JOIN destination ON resor.resorId = destination.destinationResorId WHERE resor.resorId = ?;";
+    connectionMySQL.query(sql, [id], (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
 
+function createResor(resorLand, resorDatum, resorLangd, resorPris) {
+  return new Promise((resolve, reject) => {
+    let sql =
+      "INSERT INTO resor (resorLand, resorDatum, resorLangd, resorPris) VALUES (?,?,?,?)";
+    let params = [resorLand, resorDatum, resorLangd, resorPris];
+
+    connectionMySQL.query(sql, params, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
 
 function updateResor(resorId, data) {
-    return new Promise((resolve, reject) => {
-        const sql = 'UPDATE resor SET resorLand = ?, resorDatum = ?, resaLangd = ?, resorPris = ? WHERE resorId = ?';
-        const values = [data.resorLand, data.resorDatum, data.resaLangd, data.resorPris, resorId];
-        connectionMySQL.query(sql, values, (err, result) => {
-            if (err) reject(err);
-            else resolve({ resorId, ...data });
-        });
+  return new Promise((resolve, reject) => {
+    const sql =
+      "UPDATE resor SET resorLand = ?, resorDatum = ?, resaLangd = ?, resorPris = ? WHERE resorId = ?";
+    const values = [
+      data.resorLand,
+      data.resorDatum,
+      data.resaLangd,
+      data.resorPris,
+      resorId,
+    ];
+    connectionMySQL.query(sql, values, (err, result) => {
+      if (err) reject(err);
+      else resolve({ resorId, ...data });
     });
-};
+  });
+}
 
 function deleteResa(resorId) {
-    return new Promise((resolve, reject) => {
-        const sql = 'DELETE FROM resor WHERE resorId = ?';
-        connectionMySQL.query(sql, [resorId], (err, result) => {
-            if (err) reject(err);
-            else resolve({ message: 'Resa borttagen', resorId });
-        });
+  return new Promise((resolve, reject) => {
+    const sql = "DELETE FROM resor WHERE resorId = ?";
+    connectionMySQL.query(sql, [resorId], (err, result) => {
+      if (err) reject(err);
+      else resolve({ message: "Resa borttagen", resorId });
     });
+  });
 }
 
-module.exports={
-    getResor,
-    getResa,
-    createResor,
-    updateResor,
-    deleteResa,
-}
+module.exports = {
+  getResor,
+  getResa,
+  createResor,
+  updateResor,
+  deleteResa,
+  getResaDestination,
+  getResorDestination,
+};
