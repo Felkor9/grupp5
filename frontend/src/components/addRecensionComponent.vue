3bf084b38cf1e4452199bb619cc4ced77c7a6eaa
felkor0 <felixkorshavn@gmail.com>
2025-05-07 13:35:46 +0200

    knapp är fixed

--- frontend/src/components/addRecensionComponent.vue
+++ frontend/src/components/addRecensionComponent.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
 	<div>
-		<button class="add-button"><p>+</p></button>
+		<button type="action" class="add-button"><p>+</p></button>
 	</div>
 </template>
 
@@ -10,8 +10,8 @@ div {
 	width: 100%;
 	display: flex;
 	justify-content: center;
-	background-color: #f5f7fa;
-	min-height: 50vh;
+	background-color: #f5f7fa00;
+	/* min-height: 50vh; */
 	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
 	margin: 0;
 }
@@ -22,6 +22,7 @@ div {
 	border: 1px solid black;
 	cursor: pointer;
 	margin-top: 0;
+	margin-bottom: 50px;
 	box-shadow: 0.1px 0.1px 0.4em rgb(185, 185, 185);
 }
 
