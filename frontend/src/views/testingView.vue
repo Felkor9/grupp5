4d4dce06106b29c8153bc14812dcad7fd16a926f
felkor0 <felixkorshavn@gmail.com>
2025-05-07 13:23:13 +0200

    test

---
+++ frontend/src/views/testingView.vue
@@ -0,0 +1,9 @@
+<template>
+	<!-- <h1>Scandic Strandgatan</h1> -->
+	<recensionComponent />
+	<addRecensionComponent />
+</template>
+<script setup>
+import addRecensionComponent from "../components/addRecensionComponent.vue";
+import recensionComponent from "../components/recensionComponent.vue";
+</script>
